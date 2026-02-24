0ca53e71a1dd21a902ff730fe39808473edaaaa7
Tushar Biswas <tusarbiswas888@gmail.com>
2026-02-25 00:16:14 +0600

    fix: removed loading from root layout

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from '@/components/ui/spinner';
-
-export default function Loading() {
-    return (
-        <div className="container mx-auto flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-8 h-screen animate-in fade-in duration-300">
-            <Spinner className='w-8 h-8' />
-        </div>
-    );
-}
